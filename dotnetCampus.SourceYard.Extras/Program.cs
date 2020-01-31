d2650aee5268c3199c0e03836eeb6a474b51618c
lindexi <lindexi_gd@163.com>
2020-01-31 20:26:55 +0800

    新建代码

---
+++ dotnetCampus.SourceYard.Extras/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetCampus.SourceYard.Extras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
